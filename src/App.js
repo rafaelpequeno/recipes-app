fd590cc85b66de30dd08b71c636189b63d767c55
Rafael <rflpeq.rs@gmail.com>
2023-06-05 16:44:34 -0300

    group-8__request-done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
   return (
 
     <div>
       <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/meals" component={ Recipes } />
-        <Route path="/drinks" component={ Recipes } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/done-recipes" component={ Ranking } />
-        <Route path="/favorite-recipes" component={ Ranking } />
+        {/* <Route exact path="/" component={ Login } /> */}
+        {/* <Route path="/meals" component={ Recipes } /> */}
+        <Route path="/meals/:id" component={ RecipeDetails } />
+        {/* <Route path="/drinks" component={ Recipes } /> */}
+        <Route path="/drinks/:id" component={ RecipeDetails } />
+        {/* <Route path="/profile" component={ Profile } /> */}
+        {/* <Route path="/done-recipes" component={ Ranking } /> */}
+        {/* <Route path="/favorite-recipes" component={ Ranking } /> */}
       </Switch>
     </div>
 
