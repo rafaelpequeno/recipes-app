74bda0d7fa60b58aef66d939e22def0950135870
Igor <eng.igorwatanabe@gmail.com>
2023-06-06 12:38:32 -0300

    group-8__refactoring-req2-req6

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,59 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({ email: '', password: '' });
   const [buttonStatus, setButtonStatus] = useState(true);
   const history = useHistory();
 
   const verifyButton = () => {
     const minLength = 6;
-    const passwordCheck = password.length >= minLength;
+    const passwordCheck = user.password.length > minLength;
     const validationRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    const validateEmail = validationRegex.test(email);
+    const validateEmail = validationRegex.test(user.email);
     const validateAll = passwordCheck && validateEmail;
     setButtonStatus(!validateAll);
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    if (name === 'email') {
-      setEmail(value);
-    }
-    if (name === 'password') {
-      setPassword(value);
-    }
-
+  useEffect(() => {
     verifyButton();
+  }, [user]);
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const submitButton = () => {
-    localStorage.setItem('user', JSON.stringify({ email }));
+    localStorage.setItem('user', JSON.stringify({ email: user.email }));
     history.push('/meals');
   };
 
   return (
     <div>
       <h2>LOGIN</h2>
-
       <form>
         <input
           type="text"
           placeholder="Email"
           data-testid="email-input"
           name="email"
-          value={ email }
+          value={ user.email }
           onChange={ handleChange }
         />
 
         <input
-          type="text"
+          type="password"
           placeholder="Password"
           data-testid="password-input"
           name="password"
-          value={ password }
+          value={ user.password }
           onChange={ handleChange }
         />
 
@@ -64,12 +60,9 @@ function Login() {
           onClick={ submitButton }
         >
           ENTER
-
         </button>
       </form>
-
     </div>
-
   );
 }
 
