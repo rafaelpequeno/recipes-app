4ff63d49205f287a12776b43d546a5f4c130cdb1
Matheus Arantes <matheusarantes10@hotmail.com>
2023-06-06 02:51:08 -0300

    RecipesDone page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import DoneRecipes from './pages/DoneRecipes';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
         <Route path="/meals" component={ Recipes } />
         <Route path="/drinks" component={ Recipes } />
         <Route path="/profile" component={ Profile } />
-        <Route path="/done-recipes" component={ Ranking } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/favorite-recipes" component={ Ranking } />
       </Switch>
     </div>
