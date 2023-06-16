a62ed1d6651c54d7f5477e1695869c7c8b1f8838
Rafael <rflpeq.rs@gmail.com>
2023-06-15 22:08:26 -0300

    recipeDetails-component-upgraded

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,8 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick-theme.css';
-import 'slick-carousel/slick/slick.css';
 import clipboardCopy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 import filledHeart from '../images/blackHeartIcon.svg';
@@ -12,6 +9,10 @@ import { fetchRecipeDetails } from '../services/fetchMealDetails';
 import '../styles/MealDetails.css';
 import '../styles/DrinkDetails.css';
 import '../styles/StartRecipeBTN.css';
+import { drinkAPI } from '../services/drinkAPI';
+import { mealAPI } from '../services/mealAPI';
+import YoutubeVideo from '../components/YoutubeVideo';
+import Carousel from '../components/Carousel';
 
 function RecipeDetails() {
   const {
@@ -19,37 +20,35 @@ function RecipeDetails() {
     setDoneRecipe,
     btnText,
     setBTNText,
+    youtubetLink,
+    setYoutubeLink,
+    carouselData,
+    setCarouselData,
   } = useContext(myContext);
 
   const [recipeData, setRecipeData] = useState([]);
-  const [carouselData, setCarouselData] = useState([]);
   const [CopyMessage, setCopyMessage] = useState(false);
   const [textToCopy, setTextToCopy] = useState('');
   const [favoriteIcon, setFavoriteIcon] = useState(false);
   const [route, setRoute] = useState();
-  const [youtubetLink, setYoutubeLink] = useState('');
 
   const { location: { pathname } } = useHistory();
 
   const history = useHistory();
   const { id } = useParams();
 
-  const carouselLength = 6;
-
-  const updateLink = (oldLink) => oldLink.replace('watch', 'embed').replace(/\?v=/g, '/');
-
   const apiData = async () => {
     if (pathname.includes('meals')) {
       const mealRecipe = await fetchRecipeDetails(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-      const drinkCarousel = await fetchRecipeDetails('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const drinkCarousel = await drinkAPI();
       setRecipeData(mealRecipe.meals[0]);
       setYoutubeLink(mealRecipe.meals[0].strYoutube);
-      setCarouselData(drinkCarousel.drinks);
+      setCarouselData(drinkCarousel);
     } else {
       const drinkRecipe = await fetchRecipeDetails(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-      const mealCarousel = await fetchRecipeDetails('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const mealCarousel = await mealAPI();
       setRecipeData(drinkRecipe.drinks[0]);
-      setCarouselData(mealCarousel.meals);
+      setCarouselData(mealCarousel);
     }
   };
 
@@ -83,14 +82,6 @@ function RecipeDetails() {
     setRoute(routeData);
     setTextToCopy(`http://localhost:3000/${route}/${id}`);
   }, [id, route]);
-
-  const settings = {
-    arrows: false,
-    dots: true,
-    infinite: false,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-  };
 
   const handleCopy = () => {
     setCopyMessage(true);
@@ -185,45 +176,9 @@ function RecipeDetails() {
           >
             {recipeData.strInstructions}
           </p>
-          {youtubetLink && (
-            <iframe
-              data-testid="video"
-              width="560"
-              height="315"
-              src={ updateLink(youtubetLink) }
-              title="YouTube video player"
-              allow="accelerometer"
-              allowFullScreen
-              className="meals-details-video"
-            />)}
+          {youtubetLink && <YoutubeVideo />}
           {carouselData && (
-            <span className="meals-details-carousel">
-              <h2 className="meals-details-h2-title">Recommended</h2>
-              <Slider { ...settings }>
-                {carouselData
-                  .slice(0, carouselLength)
-                  .map((recipe, index) => (
-                    <div
-                      key={ recipe.idMeal || recipe.idDrink }
-                      data-testid={ `${index}-recommendation-card` }
-                    >
-                      <span>
-                        <img
-                          className="d-block w-100 meals-details-picture-carousel"
-                          src={ recipe.strMealThumb || recipe.strDrinkThumb }
-                          alt="Recipe thumb"
-                          width="140"
-                        />
-                        <h3
-                          data-testid={ `${index}-recommendation-title` }
-                        >
-                          {recipe.strMeal || recipe.strDrink }
-                        </h3>
-                      </span>
-                    </div>
-                  ))}
-              </Slider>
-            </span>
+            <Carousel />
           )}
           {!doneRecipe && (
             <button
